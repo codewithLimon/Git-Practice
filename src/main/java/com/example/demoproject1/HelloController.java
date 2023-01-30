938c2eddbd6cec167c79da72dc2156d776825a4b
Limon <klimon221@bscse.uiu.ac.bd>
2023-01-30 15:54:56 +0600

    counting app created

--- src/main/java/com/example/demoproject1/HelloController.java
+++ src/main/java/com/example/demoproject1/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.demoproject1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button countbutton;
+
+    @FXML
+    private Label label1;
+    @FXML
+    protected Button reset;
+
+    private static int count=0;
+
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        count++;
+        label1.setText(""+count);
+    }
+
+    public void resetClicked(){
+        count=0;
+        label1.setText("0");
     }
 }
